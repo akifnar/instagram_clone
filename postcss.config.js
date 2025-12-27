a0f77410cf54123f19334212af4c5cb920e0cea0
akifnar <akifnar@gmail.com>
2025-12-27 18:00:57 +0300

    kurulumlar yapıldı

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// Not: import yerine const/require kullanıyoruz.
+const postcssGlobalData = require("@csstools/postcss-global-data");
+const postcssNested = require("postcss-nested");
+const postcssCustomMedia = require("postcss-custom-media");
+
+module.exports = {
+  plugins: [
+    postcssGlobalData({
+      files: ["./src/assets/media.css"],
+    }),
+    postcssNested(),
+    postcssCustomMedia(),
+  ],
+};
